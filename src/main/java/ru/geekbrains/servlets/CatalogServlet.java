8009d4f9f5b9f86e147150502e9f4fe01fada8a3
alexey <vadi2016@outlook.com>
2019-07-27 02:50:51 +0300

    Added servlets, filter, error, dbconnection

---
+++ src/main/java/ru/geekbrains/servlets/CatalogServlet.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.servlets;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/catalog")
+public class CatalogServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Каталог</h1>");
+        resp.getWriter().println("<a href=\"main\">Главная</a>");
+        resp.getWriter().println("<a href=\"catalog\">Каталог</a>");
+        resp.getWriter().println("<a href=\"product\">Товар</a>");
+        resp.getWriter().println("<a href=\"cart\">Корзина</a>");
+        resp.getWriter().println("<a href=\"order\">Оформление заказа</a>");
+    }
+}
